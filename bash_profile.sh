b6eae9341a391a4527bee92695f30aba51f427de
DhayaaAnbajagane <dhayaaanbajagane@gmail.com>
2024-03-12 17:20:38 -0500

    Adding Env variables to remove relative paths

---
+++ bash_profile.sh
@@ -0,0 +1,8 @@
+
+
+if [ "$USER" == "dhayaa" ]
+then
+    CSC_SRC_DIR=/home/dhayaa/DECADE/CosmicShearCosmosis
+    COSMOSIS_SRC_DIR=/home/dhayaa/DECADE/cosmosis-standard-library
+    OUTDIR=/project/chihway/dhayaa/DECADE/cosmosis
+fi
