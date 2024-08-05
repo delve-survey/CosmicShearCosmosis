969bdb14bcdf34318b016cbc8bc74e04c3a06bf5
Chihway Chang <chihway@midway3-login4.rcc.local>
2024-08-05 13:02:51 -0500

    update blinding

--- make_datavectors/MakeBlindedDV.sh
+++ make_datavectors/MakeBlindedDV.sh
@@ -7,10 +7,10 @@
 #
 # To unblind, we would just comment out Step 2 and 3
 
-sim_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/baseline_Jan12th24.fits'
+sim_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/baseline_April23rd2024.fits'
 data_dir='/project/chihway/chihway/shearcat/shear_catalog/datavec/'
-unblinded_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/data_04052024.fits'
-blinded_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/data_04052024_BLINDED.fits'
+unblinded_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/data_08052024-v2.fits'
+blinded_dv='/project/chihway/chihway/CosmicShearCosmosis/datavectors/data_08052024-v2_BLINDED.fits'
 
 # it is a little strange that it seems like we need to be in a large node to do this
 #cd /project/chihway/chihway/CosmicShearCosmosis/
@@ -23,7 +23,7 @@ python MakeBlindedDV.py $sim_dv $data_dir $unblinded_dv
 # Step 2
 echo 'step 2'
 cd ../blinding
-python -m blind_2pt_cosmosis -i blind_delve_fiducial.ini -u $unblinded_dv
+python -m blind_2pt_cosmosis -i blind_delve_fiducial.ini -u $unblinded_dv -s "cosmic_shear_of_the_decade"
 
 # Step 3
 echo 'step 3'
